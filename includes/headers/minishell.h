4329a831a0293651764440c1a30d1460d6a36afb
namenega <mene.nath@protonmail.com>
2021-08-16 16:03:47 +0200

    first commit

---
+++ includes/headers/minishell.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pyg <pyg@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/16 15:47:37 by pyg               #+#    #+#             */
+/*   Updated: 2021/08/16 15:55:57 by pyg              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/wait.h>
+# include <stdio.h>
+# include <string.h>
+# include "../libft/libft.h"
+
+typedef struct	s_ms
+{
+	int	i;
+}				t_ms;
+
+
+/*
+** MAIN_C
+*/
+
+int	main(int ac, char **av, char **envp);
+
+#endif
