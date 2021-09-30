c84233eeaa7610c9037108ebf5b0a07d2df3d517
Pyg Argus <mene.nath@protonmail.com>
2021-09-30 17:12:20 +0200

    MiniShell_TD : add builtins/echo.c - no test yet

--- test_minish/ft_echo.c
+++ test_minish/ft_echo.c
@@ -6,15 +6,17 @@
 /*   By: namenega <namenega@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/17 15:33:47 by namenega          #+#    #+#             */
-/*   Updated: 2021/09/28 14:25:48 by namenega         ###   ########.fr       */
+/*   Updated: 2021/09/30 15:01:56 by namenega         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/msh.h"
 
-//! echo + nothing = display a \n
-//! echo + $wrong_thing = display a \n
+//! echo + -n + smtg = display without \n
+//! echo + -n + nothing = display nothing
 //! echo $ = $
+//! echo $_ = un truc chelou
+//! echo + $wrong_thing = display a \n
 //! echo $'t' = t
 //! echo $"t" = t
 
