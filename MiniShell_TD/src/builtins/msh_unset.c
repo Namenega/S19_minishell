b963c464a3891a82a0455ae5ed16d73c117d578e
Pyg Argus <mene.nath@protonmail.com>
2021-10-04 18:44:35 +0200

    MiniShell_TD : src/builtins/unset.c should be ok

--- MiniShell_TD/src/builtins/msh_unset.c
+++ MiniShell_TD/src/builtins/msh_unset.c
@@ -6,7 +6,7 @@
 /*   By: namenega <namenega@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/29 09:46:24 by tderwedu          #+#    #+#             */
-/*   Updated: 2021/10/04 18:31:49 by namenega         ###   ########.fr       */
+/*   Updated: 2021/10/04 18:43:41 by namenega         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,19 +27,32 @@ static void	find_env(char *s, char **env, int len)
 	int		i;
 	char	*tmp;
 
-	i = 0;
 	tmp = NULL;
+
+
+	// i = 0;
+	// printf("--------------\n");
+	// printf("[BEFORE]]\n");
+	// while (env[i])
+	// {
+	// 	printf("-env[i] = [%s]\n", env[i]);
+	// 	i++;
+	// }
+	// printf("\n\n\n\n");
+
+
+
+
+
+	i = 0;
 	while (env[i])
 	{
 		if (!ft_strncmp(env[i], s, len))
-		{
-			printf("--env[i] = [%s]\n", env[i]);
-			// tmp = env[i];
-			// del_env(tmp, env);
 			break ;
-		}
 		i++;
 	}
+	// printf("i = %d\n", i);
+	// printf("VAR : -env[i] = [%s]\n", env[i]);
 	while (env[i] && env[i + 1])
 	{
 		tmp = env[i];
@@ -47,10 +60,20 @@ static void	find_env(char *s, char **env, int len)
 		env[i + 1] = tmp;
 		i++;
 	}
-	tmp = env[i];
-	printf("env[i] = [%s]\n", env[i]);
+	// printf("i = %d\n", i);
 	env[i] = NULL;
-	printf("-env[i] = [%s]\n", env[i]);
+
+
+
+	// printf("\n\n\n\n");
+	// printf("--------------\n");
+	// printf("[AFTER]]\n");
+	// i = 0;
+	// while (env[i])
+	// {
+	// 	printf("-env[i] = [%s]\n", env[i]);
+	// 	i++;
+	// }
 }
 
 void	msh_unset(char **env, t_exec *exec)
