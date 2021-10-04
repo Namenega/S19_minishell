ca8ed0e4bed0534e44dcf4ef77cfbbd779836242
Thibaut Derweduwen <tderwedu@fu-r7-p4.s19.be>
2021-10-04 15:26:38 +0200

    MiniShell: corr vec 50%

--- MiniShell_TD/libft/ft_vec_check.c
+++ MiniShell_TD/libft/ft_vec_check.c
@@ -6,7 +6,7 @@
 /*   By: tderwedu <tderwedu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/24 12:09:17 by tderwedu          #+#    #+#             */
-/*   Updated: 2021/09/29 15:49:43 by tderwedu         ###   ########.fr       */
+/*   Updated: 2021/10/04 15:23:47 by tderwedu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	ft_vec_check(t_vec *vec, char *str)
 	if ((vec->end - vec->ptr) < len)
 		return (0);
 	size = vec->end - vec->str;
-	while ((size - (vec->ptr - vec->str)) < len)
+	while ((size - (vec->ptr - vec->str)) < (len + 1))
 		size *= 2;
 	new = malloc(size);
 	if (!new)
@@ -32,9 +32,10 @@ int	ft_vec_check(t_vec *vec, char *str)
 	}
 	len = vec->ptr - vec->str;
 	ft_memcpy(new, vec->str, len);
+	new[len] = '\0';
 	free(vec->str);
 	vec->str = new;
-	vec->ptr = new + len;
-	vec->end = new + len;
+	vec->ptr = new + len + 1;
+	vec->end = new + size;
 	return (0);
 }
